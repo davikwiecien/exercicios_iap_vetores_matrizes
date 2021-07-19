62a3fbf9a8a2ad682ba6d2d56d136bd60722fbfd
davikwiecien <66526045+davikwiecien@users.noreply.github.com>
2021-07-19 12:40:19 -0300

    Add files via upload

---
+++ exe_5.c
@@ -0,0 +1,28 @@
+/*
+Implemente em linguagem C, algoritmo que solicite uma frase e exiba na tela a
+quantidade de letras (caracteres) utilizadas na frase (não utilize funções definidas
+em bibliotecas).
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {   
+
+    int i;
+    char frase[50];
+
+    printf("\nInforme uma frase: ");
+    fgets(frase, 50, stdin);
+    strtok(frase, "\n");
+
+    for (i = 0; frase[i] != '\0'; i++) {}
+
+    printf("\nTamanho da frase \"%s\": %d letras\n\n", frase, i);
+    
+    return 0;
+    
+}
+
+
